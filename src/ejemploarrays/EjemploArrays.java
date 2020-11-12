b6891b826a0fd8d9ec5e333a520cc978e741b9fc
Antonio <rda656@gmail.com>
2020-11-12 09:31:23 +0100

    Primer ejemplo de arrays

---
+++ src/ejemploarrays/EjemploArrays.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploarrays;
+
+/**
+ *
+ * @author profesor
+ */
+public class EjemploArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        int positivos = 0, negativos = 0;
+        
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] = ES.leerEntero("Intrdozca un número entero: ");
+        }
+        
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] < 0)
+                negativos++;
+            else
+                positivos++;
+        }
+        
+        System.out.format("En el array hay %d números positivos y %d números negativos", 
+                positivos, negativos);
+    }
+    
+}
