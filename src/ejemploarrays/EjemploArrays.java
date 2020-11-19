b7f4282a44612afb6c18be340b9812255d8417c9
Antonio <rda656@gmail.com>
2020-11-19 19:10:55 +0100

    Separación en métodos y ver el desplazamiento del contenido de un array

--- src/ejemploarrays/EjemploArrays.java
+++ src/ejemploarrays/EjemploArrays.java
@@ -15,22 +15,42 @@ public class EjemploArrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int[] valores = new int[5];
+
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = ES.leerEntero("Introduzca un valor: ");
+        }
+
+        desplazarContenidoArray(valores);
+    }
+
+    private static void desplazarContenidoArray(int[] numeros) {
+        int vUltimo = numeros[numeros.length - 1];
+
+        for (int i = numeros.length - 1; i > 0; i--) {
+            numeros[i] = numeros[i - 1];
+        }
+        numeros[0] = vUltimo;
+
+    }
+
+    private static void ejemploArray() {
         int[] numeros = new int[10];
         int positivos = 0, negativos = 0;
-        
-        for(int i = 0; i < numeros.length; i++){
+
+        for (int i = 0; i < numeros.length; i++) {
             numeros[i] = ES.leerEntero("Intrdozca un número entero: ");
         }
-        
-        for(int i = 0; i < numeros.length; i++){
-            if(numeros[i] < 0)
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < 0) {
                 negativos++;
-            else
+            } else {
                 positivos++;
+            }
         }
-        
-        System.out.format("En el array hay %d números positivos y %d números negativos", 
+
+        System.out.format("En el array hay %d números positivos y %d números negativos",
                 positivos, negativos);
     }
-    
 }
